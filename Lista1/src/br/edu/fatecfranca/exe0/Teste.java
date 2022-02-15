a5ab7641cd4bb04e946c291bda78d523ad58d9ea
Daniel Facciolo Pires <Daniel.Pires@fatec-srv.local>
2022-02-15 10:05:38 -0300

    EXE0 DA LISTA 1

---
+++ Lista1/src/br/edu/fatecfranca/exe0/Teste.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.fatecfranca.exe0;
+
+/**
+ *
+ * @author daniel.pires
+ */
+public class Teste {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // criar objeto
+        Carro obj1 = new Carro("Onix", 2019, 0, false);
+        // chamar os métodos
+        obj1.mostrar();
+        obj1.ligar();
+        obj1.mostrar();
+        obj1.acelerar(40);
+        obj1.mostrar();
+        obj1.frear(10);
+        obj1.mostrar();
+        obj1.desligar();
+        obj1.mostrar();
+    
+        // treinar: crie um outro obj2 e faça conforme fizemos
+        Carro obj2 = new Carro("Palio", 2013, 0, false);
+        // chamar os métodos
+        obj2.mostrar();
+        obj2.ligar();
+        obj2.mostrar();
+        obj2.acelerar(90);
+        obj2.mostrar();
+        obj2.frear(30);
+        obj2.mostrar();
+        obj2.desligar();
+        obj2.mostrar();
+    }
+    
+}
