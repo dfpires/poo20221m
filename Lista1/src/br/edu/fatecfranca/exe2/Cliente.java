d8f4b7f98f326a851d96e20eea741c9b900165b3
Daniel Facciolo Pires <Daniel.Pires@fatec-srv.local>
2022-02-22 10:52:55 -0300

    Exe2 e Exe3

--- Lista1/src/br/edu/fatecfranca/exe2/Cliente.java
+++ Lista1/src/br/edu/fatecfranca/exe2/Cliente.java
@@ -1,14 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.edu.fatecfranca.exe2;
 
-/**
- *
- * @author daniel.pires
- */
+import javax.swing.JOptionPane;
+
 public class Cliente {
+
+    int nroAgencia, nroConta;
+    String nome;
+    float saldo;
     
+    public Cliente(){
+        
+    }
+    public Cliente(int nroAgencia, int nroConta, String nome, float saldo){
+        this.nroAgencia = nroAgencia;
+        this.nroConta = nroConta;
+        this.nome = nome;
+        this.saldo = saldo;
+    }
+    
+    public void sacar(float x){
+        this.saldo -=x;
+    }
+    
+    public void depositar(float x){
+        this.saldo +=x;
+    }
+    
+     public void mostrar(){
+        JOptionPane.showMessageDialog(null, "Nro conta: " + this.nroConta + 
+                " Nome: " + this.nome + " Saldo " + this.saldo); 
+    }
 }
