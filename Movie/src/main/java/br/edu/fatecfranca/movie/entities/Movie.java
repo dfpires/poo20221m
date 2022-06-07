af50a14db40610c69b1ea1ddd686b49e87f7aba0
Daniel Pires <dfpires@gmail.com>
2022-06-07 08:16:13 -0300

    07-06

--- Movie/src/main/java/br/edu/fatecfranca/movie/entities/Movie.java
+++ Movie/src/main/java/br/edu/fatecfranca/movie/entities/Movie.java
@@ -1,5 +1,16 @@
 package br.edu.fatecfranca.movie.entities;
 
-public class Movie {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="tb_movie")
+public class Movie {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 }
