ebc3b90fa7441d50260e2b9953b0c3bcd121ffdb
Daniel Pires <dfpires@gmail.com>
2022-06-07 08:43:13 -0300

    07-06

--- Movie/src/main/java/br/edu/fatecfranca/movie/dtos/MovieDTO.java
+++ Movie/src/main/java/br/edu/fatecfranca/movie/dtos/MovieDTO.java
@@ -2,4 +2,63 @@ package br.edu.fatecfranca.movie.dtos;
 
 public class MovieDTO {
 
+	private Long id;
+	private String title;
+	private Double score;
+	private Integer count;
+	private String image;
+	
+	public MovieDTO() {
+		
+	}
+	
+	public MovieDTO(Long id, String title, Double score, Integer count, String image) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.score = score;
+		this.count = count;
+		this.image = image;
+	}
+	
+	public MovieDTO(MovieDTO movieDTO) {
+		this.id = movieDTO.getId();
+		this.title = movieDTO.getTitle();
+		this.score = movieDTO.getScore();
+		this.count = movieDTO.getCount();
+		this.image = movieDTO.getImage();
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public Double getScore() {
+		return score;
+	}
+	public void setScore(Double score) {
+		this.score = score;
+	}
+	public Integer getCount() {
+		return count;
+	}
+	public void setCount(Integer count) {
+		this.count = count;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	
+	
 }
--- Movie/src/main/java/br/edu/fatecfranca/movie/dtos/ScoreDTO.java
+++ Movie/src/main/java/br/edu/fatecfranca/movie/dtos/ScoreDTO.java
@@ -1,5 +1,45 @@
 package br.edu.fatecfranca.movie.dtos;
 
 public class ScoreDTO {
-
+	private Long movieId;
+	private String email;
+	private Double score;
+	
+	public ScoreDTO() {
+		
+	}
+	
+	public ScoreDTO(Long movieId, String email, Double score) {
+		super();
+		this.movieId = movieId;
+		this.email = email;
+		this.score = score;
+	}
+	
+	public ScoreDTO(ScoreDTO scoreDTO) {
+		this.movieId = scoreDTO.getMovieId();
+		this.email = scoreDTO.getEmail();
+		this.score = scoreDTO.getScore();
+	}
+	
+	public Long getMovieId() {
+		return movieId;
+	}
+	public void setMovieId(Long movieId) {
+		this.movieId = movieId;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Double getScore() {
+		return score;
+	}
+	public void setScore(Double score) {
+		this.score = score;
+	}
+	
+	
 }
